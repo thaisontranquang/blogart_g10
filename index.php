622a8ed882e7dcd34e08e93f251bddb65a13549f
Thai-Son-TQ <thai-son.tran-quang@mmibordeaux.com>
2023-02-07 17:02:23 +0100

    index and popup

--- index.php
+++ index.php
@@ -9,44 +9,50 @@ sql_connect();
     </div>
 </section>
 
-<section class="new-articles">
-    <h2>Derniers articles</h2>
+<section class="news-login">
     <div class="container">
         <div class="row">
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
+            <div class="col-md-auto">
+                <h2>Derniers articles</h2>
+                <div class="card">
+                    <img src="/src/images/index.jpeg" class="card-img-top" alt="Image dernière actualité">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <h6>Card title</h6>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <a href="#" class="btn">Go somewhere <img src="/src/images/chevron_right_FILL0_wght400_GRAD0_opsz48.svg"></a>
                     </div>
                 </div>
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
+                <div class="card">
+                    <img src="/src/images/Chaton.jpeg" class="card-img-top" alt="Image avant-dernière actualité">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6>Card title</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6>Card title</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque voluptate voluptates totam deleniti amet dignissimos? Amet dolorum at similique harum aliquam aperiam excepturi necessitatibus asperiores libero. Cum recusandae magnam voluptate.</h5>
+                        <h6>par <span>Nom de l'auteur</span> le xx/xx/xx </h6>
+                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo iusto quae aliquam at deleniti, debitis tempora fuga ratione hic, atque molestiae nemo quaerat cupiditate provident qui quas! Esse, impedit earum?
+                        </p>
+                        <a href="#" class="btn">Go somewhere <img src="/src/images/chevron_right_FILL0_wght400_GRAD0_opsz48.svg"></a>
                     </div>
                 </div>
             </div>
-            <div class="col">
-                2 of 2
+            <div class="col fast-login">
+                <h3>Connectez-vous</h3>
+                <form action="" method="post">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Adresse mail</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="votreadresse@mail.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
+                    </div>
+                    <button type="submit" class="btn">Connexion</button>
+                </form>
+
+                <h3>Pas encore de compte ?</h3>
+                <a href="#"><button type="submit" class="btn">S'inscrire</button></a>
             </div>
         </div>
-        
+
     </div>
 </section>
 
