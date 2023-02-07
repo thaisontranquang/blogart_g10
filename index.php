aaa802bea575e86544a99e1e92122d9510d6c5ba
Thai-Son-TQ <thai-son.tran-quang@mmibordeaux.com>
2023-02-07 10:22:00 +0100

    index

--- index.php
+++ index.php
@@ -1,8 +1,54 @@
 <?php require_once 'header.php';
 sql_connect();
-
-print_r(curl("https://reqres.in/api/users", "POST", '{"name": "morpheus", "job": "leader"}'));
 ?>
+
+<section class="header">
+    <div class="bartdeaux-header">
+        <h1>B’Art Deaux</h1>
+        <p>Lorem ipsum dolor sit amet consectetur. Non dolor nisi id urna turpis neque. Ac enim fringilla nulla amet in lacinia est quam magna. Nulla diam ultrices at sed. Molestie mauris praesent eu accumsan est faucibus nulla massa.</p>
+    </div>
+</section>
+
+<section class="new-articles">
+    <h2>Derniers articles</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6>Card title</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6>Card title</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6>Card title</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                2 of 2
+            </div>
+        </div>
+        
+    </div>
+</section>
 
 
 <?php require_once 'footer.php'; ?>
