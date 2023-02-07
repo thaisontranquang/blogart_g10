63e6b947e6933d8d15dd46b66cf7b044e20fc552
jagerall <coralie.alexandru@mmibordeaux.com>
2023-02-07 16:36:36 +0100

    newsGwendal

--- api/member/edit.php
+++ api/member/edit.php
@@ -1,14 +1,15 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 
-
+$numMemb = $_POST['numMemb'];
 $prenomMemb = $_POST['prenomMemb'];
 $nomMemb = $_POST['nomMemb'];
 $pseudoMemb = $_POST['pseudoMemb'];
 $passMemb = $_POST['passMemb'];
 $eMailMemb = $_POST['eMailMemb'];
 
-sql_update('MEMBRE',"prenomMemb = '$prenomMemb'", "nomMemb = $nomMemb", "pseudoMemb = '$pseudoMemb'","eMailMemb = '$eMailMemb");
+
+sql_update('MEMBRE',"prenomMemb = '$prenomMemb'", "nomMemb = $nomMemb", "pseudoMemb = '$pseudoMemb'","eMailMemb = '$eMailMemb","passMemb ='$passMemb'");
 
 
 header('Location: ../../views/backend/member/list.php');
--- views/backend/member/edit.php
+++ views/backend/member/edit.php
@@ -9,6 +9,10 @@ include '../../../header.php';
 
 $numMemb = $_GET['numMemb'];
 $pseudoMemb = sql_select("MEMBRE","pseudoMemb", "numMemb = $numMemb")[0]['pseudoMemb'];
+$prenomMemb = sql_select("MEMBRE","prenomMemb", "numMemb = $numMemb")[0]['prenomMemb'];
+$nomMemb = sql_select("MEMBRE","nomMemb", "numMemb = $numMemb")[0]['nomMemb'];
+$eMailMemb = sql_select("MEMBRE","eMailMemb", "numMemb = $numMemb")[0]['eMailMemb'];
+$passMemb = sql_select("MEMBRE","passMemb", "numMemb = $numMemb")[0]['passMemb'];
 
 ?>
 
@@ -22,12 +26,24 @@ $pseudoMemb = sql_select("MEMBRE","pseudoMemb", "numMemb = $numMemb")[0]['pseudo
             <!--Form to delete a member-->
             <form action="<?php echo ROOT_URL . '/api/member/edit.php' ?>" method="post">
                 <div class="form-group">
-                    <label for="pseudoMemb">Membre</label>
+                    
+                <label for="pseudoMemb">Membre</label>
                     <input id="numMemb" class="form-control" style="display:none" type="text" name="numMemb" value="<?php echo($numMemb) ?>" readoly="readonly">
+                    
+                    <label for="prenomMemb">Prénom</label>
                     <input id="prenomMemb" class="form-control" type="text" name="prenomMemb" value="<?php echo($prenomMemb) ?>">
+                    
+                    <label for="nomMemb">Nom</label>
                     <input id="nomMemb" class="form-control" type="text" name="nomMemb" value="<?php echo($nomMemb) ?>">
+                    
+                    <label for="pseudoMemb">Pseudo</label>
                     <input id="pseudoMemb" class="form-control" type="text" name="pseudoMemb" value="<?php echo($pseudoMemb) ?>">
+                   
+                    <label for="eMailMemb">Email</label>
                     <input id="eMailMemb" class="form-control" type="text" name="eMailMemb" value="<?php echo($eMailMemb) ?>">
+                    
+                    <label for="passMemb">Password</label>
+                  
                     <input id="passMemb" class="form-control" type="text" name="passMemb" value="<?php echo($passMemb) ?>">
                 </div>
                 <div class="form-group mt-2">
