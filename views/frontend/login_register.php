22250527df12d947a267cb4e8dd328b2133c958e
MatthieuMarchand <matthieu.marchand@mmibordeaux.com>
2023-02-08 17:43:37 +0100

    register v1

--- views/frontend/login_register.php
+++ views/frontend/login_register.php
@@ -1,41 +1,18 @@
-<?php require_once '../../header.php';
-sql_connect();
-
-sql_select('MEMBRE', "'eMailMemb','passMemb'")
-
-session_start();
-if(isset($_POST['eMailMemb']) && isset($_POST['passMemb']))
-{
- 
-    if($username !== "" && $password !== "")
-    {
-        $requete = "SELECT count(*) FROM utilisateur where 
-        nom_utilisateur = '".$username."' and mot_de_passe = '".$password."' ";
-        $exec_requete = mysqli_query($db,$requete);
-        $reponse = mysqli_fetch_array($exec_requete);
-        $count = $reponse['count(*)'];
-            if($count!=0) // nom d'utilisateur et mot de passe correctes
-            {
-            $_SESSION['username'] = $username;
-            header('Location: principale.php');
-            }
-            else
-            {
-            header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
-            }
-            }
-            else
-            {
-            header('Location: login.php?erreur=2'); // utilisateur ou mot de passe vide
-            }
-            }
-            else{
-        header('Location: login.php');
-    }
-mysqli_close($db); // fermer la connexion
+<?php
+include '../../header.php';
 ?>
 
+<form action="<?php echo ROOT_URL . '/api/register/register.php' ?>" method="post">
+    <div class="form-group">
+        <label for="eMailMemb">Email</label>
+        <input id="eMailMemb" class="form-control" type="text" name="eMailMemb" placeholder="Entrez votre email" required>
+    </div>
+    <div class="form-group">
+        <label for="passMemb">Mot de passe</label>
+        <input id="passMemb" class="form-control" type="password" name="passMemb" placeholder="Entrez votre mot de passe" required>
+    </div>
 
-?>
-
-<?php require_once '../../footer.php';
+    <div class="form-group mt-2">
+        <button type="submit" class="btn btn-primary">Se connecter</button>
+    </div>
+</form>
